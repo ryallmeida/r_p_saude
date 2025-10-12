4ccb6cb51ddb14ed7a4dbd6134bb917d07096445
ryallmeida <ryallmeida@gmail.com>
2025-10-12 19:09:26 -0300

    changes

--- eda_obesity1.R
+++ eda_obesity1.R
@@ -10,7 +10,7 @@
 # -------------------------------------
 # CODADO ORIGINALMENTE EM R, V. 4.4.1 EM 11 DE OUTUBRO DE 2025
 
-# HIPOTESE: COMO DESCREVER ESSA AMOSTRA EM TERMOS DE CENTRALIDADE, DISPERSÃO, FORMA E FREQUÊNICA?
+# HIPOTESE: COMO DESCREVER ESSA AMOSTRA EM TERMOS DE CENTRALIDADE, DISPERSÃO, FORMA E FREQUÊNCIA?
 
 # =====================================
 # IMPORTANDO BIBLIOTECAS
@@ -24,28 +24,38 @@ pacman::p_load(tidyverse,
                likert, 
                table1, 
                flextable, 
-               RColorBrewer)
+               RColorBrewer, 
+               viridis)
 
 # =====================================
 # IMPORTANDO DADOS
 
 arquivos <- unzip("C:/Users/Notebook/Downloads/DOC-20250921-WA0022.zip", 
                   list = TRUE)
-print(arquivos)
+# print(arquivos)
 
 dados <- read.csv(unz("C:/Users/Notebook/Downloads/DOC-20250921-WA0022.zip", "ObesityDataSet_raw_and_data_sinthetic.csv"))
 
 # =============================================================================
 # ANÁLISE EXPLORATÓRIA DE DADOS
 # =============================================================================
+
+# NOTA ------------------------------------------------------------------------
+# LEMBRE-SE QUE HÁ VARIÁVEIS QUALITATIVAS NOMINAIS E ORDINAIS E QUANTITATIVAS DISCRETA E CONTINUA 
+
+# VARIAVEIS DICOTÔMICAS-BINÁRIAS TEORICAMENTE SÃO VARIAVEIS QUALITATIVAS NOMINAIS POIS NÃO HA UMA ORDEM NATURAL-TEÓRICA PARA DESCREVÊ-LA, ENTRENTANTO PARA MODELAGENS ELA DEVE SER TRATADAS COMO UMA VARIAVEL QUANTITATIVA DISCRETA (VIA TRATAMENTO ESTATISTICO)
+
 view(dados)
+# HIPOTESE: COMO A MAQUINA ESTA LENDO ESSAS VARIÁVEIS?
 dplyr::glimpse(dados)
 
 # TRANSFORMAÇÕES BÁSICAS
 dados$Gender <- as.factor(dados$Gender)
 dados$Age <- as.integer(dados$Age)
-
+# =============================================================================
 # dados$Gender ---------------------------------------------------------------
+# =============================================================================
+# VARIÁVEL CATEGÓRICA DICOTÔMICA
 
 plot(dados$Gender)
 
@@ -57,7 +67,10 @@ prop.table(table(dados$Gender))
 #    Female      Male 
 # 0.4940786 0.5059214 
 
+# =============================================================================
 # dados$Age -------------------------------------------------------------------
+# =============================================================================
+# VARIÁVEL CONTINUA DISCRETA
 
 hist(dados$Age, include.lowest = T)
 # Observa-se que a amostra gira em torno de pessoas de aproximadamente 15 a 45 anos 
@@ -85,7 +98,64 @@ view(df_prop)
 round(100 * prop.table(table(cut(dados$Age, 
                                  breaks = grDevices::nclass.Sturges(dados$Age)))), 2)
 
+# VARIAVEL CATEGORICA (GENDER)  X VARIAVE QUANT DISCRETA (AGE)
+
+dados$Gender <- as.factor(dados$Gender)
+
+ggplot2::ggplot(dados, aes(x = Age,
+                           fill = Gender)) +
+  geom_density(alpha = 0.5, 
+               color = NA) +
+  labs(title = "", 
+       x = "Idade",
+       y = "Densidade") + 
+  theme_gray()
+
 # ALTURA ----------------------------------------------------------------------
+# VARIÁVEL CONTINUA 
+
+p1 <- dados %>%
+  ggplot( aes(x= Gender, 
+              y= Height, 
+              fill= Gender)) +
+  geom_violin(alpha = 0.2, 
+              scale = "width") +
+  geom_boxplot() +
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  geom_jitter(color="black", 
+              size=0.2, 
+              alpha=0.7,
+              width = 0.4,
+              shape = 10) +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  ) +
+  ggtitle("") +
+  xlab("")
+
+# ggsave("C:/Users/Notebook/Downloads/bloxplot1.png", plot = p1, width = 10, height = 6, dpi = 300)
+
+p1_void <- dados %>%
+  ggplot( aes(x= Gender, 
+              y= Height, 
+              fill= Gender)) +
+  geom_violin(alpha = 0.2, 
+              scale = "width") +
+  geom_boxplot() +
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  geom_jitter(color="black", 
+              size=0.2, 
+              alpha=0.7,
+              width = 0.4,
+              shape = 10) +
+  theme_void() +
+  ggtitle("") +
+  xlab("")
+
+# ggsave("C:/Users/Notebook/Downloads/bloxplot1_void.png", plot = p1_void, width = 10, height = 6, dpi = 300)
 
 summary(dados$Height)
 #  Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
@@ -100,6 +170,52 @@ rug(dados$Height)
 boxplot(dados$Height)
 
 # PESO -----------------------------------------------------------------------
+# VARIÁVEL CONTINUA
+
+p2 <- dados %>%
+  ggplot( aes(x= Gender, 
+              y= Weight, 
+              fill= Gender)) +
+  geom_violin(alpha = 0.2, 
+              scale = "width") +
+  geom_boxplot() +
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  geom_jitter(color="black", 
+              size=0.2, 
+              alpha=0.7,
+              width = 0.4,
+              shape = 10) +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  ) +
+  ggtitle("") +
+  xlab("")
+
+print(p2)
+
+# ggsave("C:/Users/Notebook/Downloads/bloxplot_peso.png", plot = p2, width = 10, height = 6, dpi = 300)
+
+p2_void <- dados %>%
+  ggplot( aes(x= Gender, 
+              y= Weight, 
+              fill= Gender)) +
+  geom_violin(alpha = 0.2, 
+              scale = "width") +
+  geom_boxplot() +
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  geom_jitter(color="black", 
+              size=0.2, 
+              alpha=0.7,
+              width = 0.4,
+              shape = 10) +
+  theme_void() +
+  ggtitle("") +
+  xlab("")
+
+# ggsave("C:/Users/Notebook/Downloads/bloxplot_peso_void.png", plot = p2_void, width = 10, height = 6, dpi = 300)
 
 summary(dados$Weight)
 #  Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
@@ -113,7 +229,97 @@ rug(dados$Weight)
 
 boxplot(dados$Weight)
 
+# ------------------------------------
+
+ggplot2::ggplot(dados, aes(x = Weight,
+                           fill = Gender)) +
+  geom_density(alpha = 0.5, 
+               color = NA) +
+  labs(title = "", 
+       x = "Peso",
+       y = "Densidade") + 
+  theme_gray()
+
+# PROBLEMA: PARA COMPARAR VARIAVEIS TIVE QUE NORMALIZÁ-LAS POR ZSCORE VISTO QUE PESO E ALTURA ESTAVAM EM UNIDADE DE MEDIDAS DIFERENTES
+
+Height_z <- scale(dados$Height)
+Weight_z <- scale(dados$Weight)
+
+
+p3 <- ggplot(dados) +
+  # Altura padronizada (cima)
+  geom_density(aes(x = Height_z, 
+                   y = after_stat(density)/max(after_stat(density)), 
+                   fill = Gender),
+               alpha = 0.5, 
+               color = NA) +
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  annotate("label",
+           x = 4.5,
+           y = 0.25,
+           label = "Altura",
+           color = "black", 
+           hjust = 1) +
+  
+  # Peso padronizado (baixo, espelhado)
+  geom_density(aes(x = Weight_z, y = -after_stat(density)/max(after_stat(density)), fill = Gender),
+               alpha = 0.5, 
+               color = NA) + 
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  annotate("label",
+           x = 4.5,
+           y = -0.25,
+           label = "Peso",
+           color = "black", 
+           hjust = 1) +
+  geom_hline(yintercept = 0) +
+  labs(title = "", 
+       x = "Valores padronizados (Zscore)",
+       y = "Densidade normalizada") +
+  theme_minimal()
+print(p3)
+
+#ggsave("C:/Users/Notebook/Downloads/densidade_peso_altura.png", plot = p3, width = 10, height = 6, dpi = 300)
+
+p3_void <- ggplot(dados) +
+  # Altura padronizada (cima)
+  geom_density(aes(x = Height_z, 
+                   y = after_stat(density)/max(after_stat(density)), 
+                   fill = Gender),
+               alpha = 0.5, 
+               color = NA) +
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  annotate("label",
+           x = 4.5,
+           y = 0.25,
+           label = "Altura",
+           color = "black", 
+           hjust = 1) +
+  
+  # Peso padronizado (baixo, espelhado)
+geom_density(aes(x = Weight_z, y = -after_stat(density)/max(after_stat(density)), fill = Gender),
+               alpha = 0.5, 
+               color = NA) + 
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  annotate("label",
+           x = 4.5,
+           y = -0.25,
+           label = "Peso",
+           color = "black", 
+           hjust = 1) +
+  geom_hline(yintercept = 0) +
+  theme_void()
+
+print(p3_void)
+
+# ggsave("C:/Users/Notebook/Downloads/densidade_peso_altura_void.png", plot = p3_void, width = 10, height = 6, dpi = 300)
+
 # HISTÓRICO FAMILIAR DE SOBREPESO -----------------------------------------------------------------------
+# VARIÁVEL CATEGÓRICA DICOTÔMICA
 
 plot(as.factor(dados$family_history_with_overweight))
 
@@ -134,22 +340,123 @@ print(as.data.frame(prop.table(tabela_freq3)))
 # 3   no   Male 0.0724775
 # 4  yes   Male 0.4334439
 
+tabela_freq4 <- table(dados$family_history_with_overweight, dados$NObeyesdad)
+print(as.data.frame(prop.table(tabela_freq4)))
+
+# PARA FUTURAS MODELAGENS E OUTROS TIPOS DE ANÁLISE PRECISA-SE CONVERTER OS CARACTERES EM NUMEROS, ENTÃO "YES" == 1 E "NO" == 0
+dados$family_history_with_overweight <- as.numeric(ifelse(dados$family_history_with_overweight == "yes", 1, ifelse(dados$family_history_with_overweight == "no", 0, NA)))
+head(dados$family_history_with_overweight)
+
 # Frequent consumption of high caloric food (FAVC) -----------------------------------
-hist(as.numeric(dados$FAVC))
+# VARIAVEL CATEGORICA DICOTOMICA
 
-# Frequency of consumption of vegetables (FCVC) ----------------------------------------
 
-dados$FCVC <- as.double(dados$FCVC)
+unique(dados$FAVC)
+# [1] "no"  "yes"
+
+dados$FAVC <- as.numeric(
+  ifelse(dados$FAVC == "yes", 1, 
+         ifelse(dados$FAVC == "no", 0, NA)))
+unique(dados$FAVC)
+# YES == 1
+#  NO == 0
+
+plot(factor(dados$FAVC))
+# MAJORITARIAMENTE A AMOSTRA TEM UM ALTO CONSUMO DE COMIDAS ALTAMENTE CALÓRICAS 
+
+# DADOS PROPORCIONAMENTE FALANDO
+prop.table(table(dados$FAVC))
+#       0         1 
+#0.1160587 0.8839413 
+
+
+tabela_freq5 <- table(dados$FAVC, dados$NObeyesdad)
+print(as.data.frame(prop.table(tabela_freq5)))
+
+# FREQUENCY OF CONSUMPTION OF VEGETABLES (FCVC) ----------------------------------------
+
+unique(dados$FCVC)
+
+p4 <- dados %>%
+  ggplot( aes(x= Gender, 
+              y= FCVC, 
+              fill= Gender)) +
+  geom_violin(alpha = 0.2, 
+              scale = "width") +
+  geom_boxplot() +
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  geom_jitter(color="black", 
+              size=0.2, 
+              alpha=0.7,
+              width = 0.4,
+              shape = 10) +
+  labs(title = "",
+       y = "Frequency of Comsumption of Vegetables") +
+  theme_minimal()
+  
+print(p4)
+
+# ggsave("C:/Users/Notebook/Downloads/boxplot_FCVC.png", plot = p4, width = 10, height = 6, dpi = 300)
+
 hist(as.numeric(dados$FCVC))
 
+# NUMBER OF MAIN MEALS (NCP) -----------------------------------------------------
 
-# Number  of main meals (NCP) ----------------------------------------
+p5 <- dados %>%
+  ggplot( aes(x= Gender, 
+              y= NCP, 
+              fill= Gender)) +
+  geom_violin(alpha = 0.2, 
+              scale = "width") +
+  geom_boxplot() +
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  geom_jitter(color="black", 
+              size=0.2, 
+              alpha=0.7,
+              width = 0.4,
+              shape = 10) +
+  labs(title = "",
+       y = "Number of main of meals") +
+  theme_minimal()
+
+print(p5)
+
+# ggsave("C:/Users/Notebook/Downloads/boxplot_NCp.png", plot = p5, width = 10, height = 6, dpi = 300)
+
 hist(dados$NCP)
 
-# Consuption of food between meals (CAEC) ----------------------------------------
+# CONSUPTION OF FOOD BETWEN MEALS (CAEC) ----------------------------------------
 plot(as.factor(dados$CAEC))
+unique(dados$CAEC)
+# VARIAVEL CATEGORICA ORDINAL
 
-#  Consumption of water (CH20)
+p6 <- dados %>%
+  ggplot( aes(x= Gender, 
+              y= NCP, 
+              fill= Gender)) +
+  geom_violin(alpha = 0.2, 
+              scale = "width") +
+  geom_boxplot() +
+  viridis::scale_fill_viridis(discrete = TRUE, 
+                              alpha=0.6) +
+  geom_jitter(color="black", 
+              size=0.2, 
+              alpha=0.7,
+              width = 0.4,
+              shape = 10) +
+  labs(title = "",
+       y = "Number of main of meals") +
+  theme_minimal()
+
+print(p5)
+
+# ggsave("C:/Users/Notebook/Downloads/boxplot_NCp.png", plot = p5, width = 10, height = 6, dpi = 300)
+
+
+
+# CONSUMPTION OF WATER (CH20) -----------------------------------------------------
 
 plot(as.factor(dados$SMOKE))
 
@@ -158,24 +465,24 @@ plot(as.factor(dados$SMOKE))
 
 plot(as.factor(dados$SCC))
 
-# Physical activity frequency (FAF)
+# PHYSICAL ACTIVITY FREQUENCY (FAF) -----------------------------------------------------
 
 hist(dados$FAF)
 
-# Time using technology (TUE)
+# TIME USING TECHNOLOGY (TUE) -----------------------------------------------------
 
 hist(dados$TUE)
 
-# Consumption of alcohol (CALC)
+# CONSUMTION OF ALCOHOL (CALC) -----------------------------------------------------
 
 dados$CALC <- as.factor(dados$CALC)
 plot(as.factor(dados$CALC))
 
-# Transportation used (MTRANS)
+# TRANSPORTATION USED (MTRANS) -----------------------------------------------------
 
 plot(as.factor(dados$MTRANS))
 
-# Obesity leval (NObeyesdad) ----------------------------------------------------
+# OBSESITY LEVEL (NObeyesdad) ----------------------------------------------------
 # VARIAVEL CATEGÓRICA ORDINAL
 
 prop.table(table(dados$NObeyesdad))
@@ -184,7 +491,10 @@ prop.table(table(dados$NObeyesdad))
 #  0.1534818           0.1373757           0.1373757 
 
 plot(as.factor(dados$NObeyesdad))
-
 levels(factor(dados$NObeyesdad))
 
-table1::table1(~ ., data = dados)
+# ---------------------------------------------------
+# -----------------------------------------
+
+
+
