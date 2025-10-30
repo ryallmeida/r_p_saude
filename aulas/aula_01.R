6532d3b5e524912a83002a8a6fc5121de020424c
ryallmeida <ryallmeida@gmail.com>
2025-10-30 20:10:35 -0300

    new

--- aulas/aula_01.R
+++ aulas/aula_01.R
@@ -1,7 +1,7 @@
 # UNIVERSIDADE FEDERAL DE PERNAMBUCO 
 # PROGRAMA DE PÓS-GRADUAÇÃO EM NUTRIÇÃO & DEPARTAMENTO DE CIÊNCIA POLÍTICA
 # CURSO: R PARA SAÚDE, 3 A 7 DE NOVEMBRO DE 2025
-
+# AULA 01 
 # ==============================================================================
 # CODADO ORIGINALMENTE EM R, V. 4.5.1 EM 30 DE OUTUBRO DE 2025
 # ==============================================================================
@@ -20,13 +20,25 @@ pacman::p_load(tidyverse)
 
 # ==============================================================================
 
+if(!require("MASS")) {
+  install.packages("MASS")
+}
+
+try(data(package = "MASS"), silent = TRUE)
+data(UScereal)
+
+
+
 if(!require("NHANES")) {
   install.packages("NHANES")
 }
 
+# PARA MAIS DATALHES SOBRE O BANCO DE DADOS. ACESSE:
+# https://www.cdc.gov/nchs/nhanes/about/
+
 data(NHANES)
 
-# PEÇA AJUDA AO SISTEMA 
+# PEÇA AJUDA AO SISTEMA ATRAVES DESSES CÓDIGOS
 help("NHANES", package = "NHANES")
 try(data(package = "NHANES"), silent = TRUE)
 
@@ -36,10 +48,10 @@ if(!require("Epi")) {
   install.packages("Epi")
 }
 
-data(births)
-data(melanoma)
-
 try(data(package = "Epi"), silent = TRUE)
+
+data(DMepi)
+data(diet)
 
 # ==============================================================================
 
@@ -47,21 +59,9 @@ if(!require("MASS")) {
   install.packages("MASS")
 }
 
-data(anorexia)
-
 try(data(package = "MASS"), silent = TRUE)
 
-# ==============================================================================
-
-if(!require("foodcomex")) {
-  install.packages("foodcomex")
-}
-
-library(foodcomex)
-
-try(data(package = "foodcomex"), silent = TRUE)
-
-data(food_composition)
+data(anorexia)
 
 
 
