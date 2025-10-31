8afa98ee25c39cb19396f4cd6748f0da3c04f4ea
ryallmeida <ryallmeida@gmail.com>
2025-10-30 22:36:44 -0300

    add

--- aulas/aula_01.R
+++ aulas/aula_01.R
@@ -19,14 +19,75 @@ if(!require("pacman")) {
 pacman::p_load(tidyverse)
 
 # ==============================================================================
+# AULA 01: O ZERO DO ZERO
+# ==============================================================================
+
+# TÓPICO: R COMO UMA CALCULADORA
+
+2+2
+
+2*2
+
+2/2
+
+2-2
+
+2^2
+
+# USE PARENTESE PARA SEPARAR PARTES DO CALCULO 
+
+((4+14)/4)^2
+
+# COMO ACESSAR A AJUDA NO R?
+
+help.search("logaritmo")
+help.search("logarithms")
+
+# OU
+
+??logatithms
+
+# QUAIS SÃO AS FUNÇÕES MAIS BÁSICAS DO R COMO CALCULADORA? 
+
+sqrt(9)
+sqrt(3*3^2) #27
+sqrt((3*3)^2) #81
+
+prod(2,2)
+prod(2,4,5,6,7)
+
+log(3)
+log(3, 10)
+log10(3)
+
+# abs pega valores em modulo 
+abs(3-9)
+
+# para fatoriais
+factorial(4)
+
+# COMO CRIAR FUNÇÕES NO R? 
+
+
+if(!require("MASS")) {
+  install.packages("MASS")
+}
+
+try(data(package = "MASS"), silent = TRUE)
+data(UScereal)
+
+
 
 if(!require("NHANES")) {
   install.packages("NHANES")
 }
 
+# PARA MAIS DATALHES SOBRE O BANCO DE DADOS. ACESSE:
+# https://www.cdc.gov/nchs/nhanes/about/
+
 data(NHANES)
 
-# PEÇA AJUDA AO SISTEMA 
+# PEÇA AJUDA AO SISTEMA ATRAVES DESSES CÓDIGOS
 help("NHANES", package = "NHANES")
 try(data(package = "NHANES"), silent = TRUE)
 
@@ -36,10 +97,10 @@ if(!require("Epi")) {
   install.packages("Epi")
 }
 
-data(births)
-data(melanoma)
-
 try(data(package = "Epi"), silent = TRUE)
+
+data(DMepi)
+data(diet)
 
 # ==============================================================================
 
@@ -47,21 +108,8 @@ if(!require("MASS")) {
   install.packages("MASS")
 }
 
-data(anorexia)
-
 try(data(package = "MASS"), silent = TRUE)
 
-# ==============================================================================
-
-if(!require("foodcomex")) {
-  install.packages("foodcomex")
-}
-
-library(foodcomex)
-
-try(data(package = "foodcomex"), silent = TRUE)
-
-data(food_composition)
-
+data(anorexia)
 
 
